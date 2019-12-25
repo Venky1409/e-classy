b43821f91742d747499fbd7f33d1d9fb86fb0bb1
Adigicherla <venkatesh.adigicherla@spglobal.com>
2019-12-25 19:14:24 +0530

    Changes for heroku deployment

--- server-src/middleware/app-middleware.ts
+++ server-src/middleware/app-middleware.ts
@@ -5,7 +5,7 @@ import * as bodyParser from 'body-parser';
 
 export function appMiddleware(app: Express) {
     return (req: Request, res: Response, next: NextFunction) => {
-        app.use(express.static(path.join(__dirname, '../../web/dist')));
+        app.use(express.static(path.join(__dirname, '../../dist')));
         app.use(bodyParser.urlencoded({ extended: true }));
         app.use(bodyParser.json());
         next();
