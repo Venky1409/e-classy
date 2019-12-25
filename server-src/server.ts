0cef60fe6668e81549b9c668ed5e76692569f4e0
Srikanth Guntha <srikanth0190@gmail.com>
2019-12-25 19:03:01 +0530

    committed changes of mongoose connection

--- server-src/server.ts
+++ server-src/server.ts
@@ -6,6 +6,18 @@ app.listen(port, (err) => {
   if (err) {
     return console.log(err);
   }
+  
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://admin:<admin>@cluster0-mivcc.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   console.log("connected");
+//   const collection = client.db("eclassydb").collection("categories");
+//   console.log(collection);
+ 
+//   // perform actions on the collection object
+//   client.close();
+// });
 
   return console.log(`server is listening on ${port}`);
 });
